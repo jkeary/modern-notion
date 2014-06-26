98be590ee076ece7b2c4244b92e72bc9e9937708
Mark Johnson <markandersjohnson@gmail.com>
2014-06-25 19:57:11 -0500

    homepage fix

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,4 @@
 				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
-					<?php if(is_front_page()): ?>
-						<?php get_template_part( 'partials/content', 'sidebar-small-post-list'); ?>
-					<?php endif; ?>
 					<img src="http://placehold.it/299x247&text=Advertisement" alt="" class="sidebar-advertisement">
 					<div class="hidden-xs">
 						<?php get_template_part( 'partials/content', 'sidebar-large-post-list'); ?>
