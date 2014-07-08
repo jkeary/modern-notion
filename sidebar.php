4ad53156f61a816d1b39c6a100e5f8294b8ae9aa
Mark Johnson <markandersjohnson@gmail.com>
2014-07-08 15:28:49 -0500

    misc changes

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,8 @@
 				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
-					<img src="http://placehold.it/299x247&text=Advertisement" alt="" class="sidebar-advertisement">
+					<?php if(is_front_page()): ?>
+						<?php get_template_part( 'partials/content', 'sidebar-small-post-list'); ?>
+					<?php endif; ?>
+					<img src="http://placehold.it/299x247&text=Advertisement" alt="" class="page-block">
 					<div class="hidden-xs">
 						<?php get_template_part( 'partials/content', 'sidebar-large-post-list'); ?>
 					</div>
