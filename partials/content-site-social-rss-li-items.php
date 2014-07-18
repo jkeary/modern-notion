44ea34cfde828f17894a694d3173836674df33b1
JP <jsniezek@crowdskout.com>
2014-07-18 15:22:45 -0400

    Added fb and twitter icons to nav bar

--- partials/content-site-social-rss-li-items.php
+++ partials/content-site-social-rss-li-items.php
@@ -1,9 +1,13 @@
 <li>
-	<a href="https://twitter.com/modernnotion" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @twitter</a>
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>	
+	<a href="https://twitter.com/modernnotion" target="_blank">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/twitter.png" alt="">
+	</a>
 </li>
 <li>
-	<div class="fb-like" data-href="<?php home_url(); ?>" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+	<a href="https://www.facebook.com/modernnotion" target="_blank">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/facebook.png" alt="">
+	</a>
+	<!-- <div class="fb-like" data-href="<?php home_url(); ?>" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div> -->
 </li>						
 <li class="rss-feed-item">
 	<a href="<?php bloginfo('rss2_url'); ?>" title="RSS Feed" class="icon-feed"><span class="sr-only">RSS Feed</span></a>
