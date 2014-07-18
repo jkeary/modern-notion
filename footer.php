a79808ddb762dff254e614ba8248df4e884fedea
JP <jsniezek@crowdskout.com>
2014-07-18 16:20:38 -0400

    Registered another footer nav

--- footer.php
+++ footer.php
@@ -37,6 +37,23 @@
     					)); ?>
 
     				</nav>
+
+                    <nav role="navigation" class="header-nav-in-footer">
+                        <?php wp_nav_menu(array(
+                        'container' => false,                           // remove nav container
+                        'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                        'menu' => __( 'Legal', 'bonestheme' ),  // nav name
+                        'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                        'theme_location' => 'legal',                 // where it's located in the theme
+                        'before' => '',                                 // before the menu
+                    'after' => '',                                  // after the menu
+                    'link_before' => '',                            // before each link
+                    'link_after' => '',                             // after each link
+                    'depth' => 0,                                   // limit the depth of the nav
+                        'fallback_cb' => ''                             // fallback function (if there is one)
+                        )); ?>
+
+                    </nav>                    
                                         
 
     			</div>
