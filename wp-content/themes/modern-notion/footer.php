ddc98451bf9225cf9b4f2c0807ea0a6d171b419c
JP <jsniezek@crowdskout.com>
2014-07-24 11:42:56 -0400

    Commented out crowdskout analytics

--- wp-content/themes/modern-notion/footer.php
+++ wp-content/themes/modern-notion/footer.php
@@ -90,9 +90,9 @@
     
     <!-- Crowdskout -->
     <script>(function(l,o,v,e) {
-        l.ownerid = 6
-        ;a=o.getElementsByTagName(v)[0];b=o.createElement(v);b.src=e;a.parentNode.insertBefore(b,a);
-        })(window, document, 'script', 'https://api.crowdskout.com/analytics.js');
+        // l.ownerid = 6
+        // ;a=o.getElementsByTagName(v)[0];b=o.createElement(v);b.src=e;a.parentNode.insertBefore(b,a);
+        // })(window, document, 'script', 'https://api.crowdskout.com/analytics.js');
     </script>    
 
 </body>
