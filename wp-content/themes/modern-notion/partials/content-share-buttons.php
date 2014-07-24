c57b7a8857597e512120cbfe63dbec3e59512b98
JP <jsniezek@crowdskout.com>
2014-07-24 17:13:07 -0400

    Added bitly links, not working on localhost, going to try staging

--- wp-content/themes/modern-notion/partials/content-share-buttons.php
+++ wp-content/themes/modern-notion/partials/content-share-buttons.php
@@ -1,15 +1,15 @@
 <span><a 
 	onclick="window.open(this.href, 'mywin', 'left=50,top=50,width=550,height=550,toolbar=1,resizable=0'); return false;" 
-	class="twitter" href="http://twitter.com/share?url=<?php the_permalink(); ?>&amp;text=<?php echo urlencode(the_title()); ?>" 
+	class="twitter" href="http://twitter.com/share?url=<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>&amp;text=<?php echo urlencode(the_title()); ?>" 
 	target="_blank"><span class="icon-twitter"></span><span class="text">Share on Twitter</span></a></span>
 <span><a 
 	onclick="window.open(this.href, 'mywin','left=50,top=50,width=550,height=550,toolbar=1,resizable=0'); return false;" 
-	class="facebook" href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" 
+	class="facebook" href="http://www.facebook.com/sharer.php?u=<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>" 
 	target="_blank"><span class="icon-facebook"></span><span class="text">Share on Facebook</span></a></span>
 <span><a onclick="window.open(this.href, 'mywin','left=50,top=50,width=550,height=550,toolbar=1,resizable=0'); return false;" 
-	class="linkedin" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>" 
+	class="linkedin" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>" 
 	target="_blank"><span class="icon-linkedin"></span><span class="text">Share on LinkedIn</span></a></span>
 <span><a 
 	class="email" 
-	href="mailto:?Subject=<?php the_title(); ?>&amp;Body=%20<?php the_permalink(); ?>">
+	href="mailto:?Subject=<?php the_title(); ?>&amp;Body=%20<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>">
 	<span class="icon-Email"></span><span class="sr-only">Email</span></a></span>
