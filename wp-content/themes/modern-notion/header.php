341586fdc37c56e200cd4fb2e0c0a9da1bde7020
JP <jsniezek@crowdskout.com>
2014-07-25 09:39:21 -0400

    Used permalinks for meta tags

--- wp-content/themes/modern-notion/header.php
+++ wp-content/themes/modern-notion/header.php
@@ -17,7 +17,7 @@
         <!-- FB tags -->
         <meta property="og:title" content="<?php the_title(); ?>" />
         <meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
-        <meta property="og:url" content="<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>" />
+        <meta property="og:url" content="<?php the_permalink(); ?>" />
         <meta property="fb:app_id" content="1453391324923585" />
         <meta property="og:type" content="article" />
         <meta property="og:description" content="<?php if(get_field('dek')) the_field('dek'); ?>" />
@@ -31,7 +31,7 @@
         <meta name="twitter:creator" content="@modernnotion" />
         <meta name="twitter:description" content="<?php if(get_field('dek')) the_field('dek'); ?>" />
         <meta name="twitter:image:src" content="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )[0]; ?>">        
-        <meta name="twitter:url" content="<?php echo wpbitly_get_shortlink(get_permalink(), $post->ID); ?>" />
+        <meta name="twitter:url" content="<?php the_permalink(); ?>" />
     <?php endif; ?>
 
 	<?php // mobile meta (hooray!) ?>
