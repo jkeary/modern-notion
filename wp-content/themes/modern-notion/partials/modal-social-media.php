086f8d5d060e6892c64a83e9260d0b320a5db9b4
JP <jsniezek@crowdskout.com>
2014-07-25 11:31:05 -0400

    Added text to social media modal

--- wp-content/themes/modern-notion/partials/modal-social-media.php
+++ wp-content/themes/modern-notion/partials/modal-social-media.php
@@ -11,6 +11,7 @@
         </div>
       </div>
       <div class="modal-body clearfix">
+        <p id="social-modal-intro"></p>
     		<div class="share-buttons inline-color-style">
     			<span><a class="twitter" href="https://twitter.com/modernnotion" target="_blank"><span class="icon-twitter"></span><span class="text">Follow us on Twitter</span></a></span>
     			<span><a class="facebook" href="https://facebook.com/modernnotion" target="_blank"><span class="icon-facebook"></span><span class="text">Like us on Facebook</span></a></span>
