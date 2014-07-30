1e40b3e38f0dd79cf30b43592a8cdace7deda8d7
JP <jsniezek@crowdskout.com>
2014-07-30 09:39:00 -0400

    Removed share by email button

--- wp-content/themes/modern-notion/partials/content-single-article.php
+++ wp-content/themes/modern-notion/partials/content-single-article.php
@@ -57,9 +57,7 @@
                     <?php the_field('video_embed_code'); ?>
                 </div> 
             <?php else: ?>
-                <a href="<?php the_permalink(); ?>">
         	       <?php the_post_thumbnail('large'); ?>
-               </a>
                <?php
                     $link = get_post_meta($post->ID, "cite_link", true);
                     $text = get_post_meta($post->ID, "cite_text", true);
