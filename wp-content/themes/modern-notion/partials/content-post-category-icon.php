4c0ed0c3d2e4f1db4a2f669accd898b80b6cd839
JP <jsniezek@crowdskout.com>
2014-07-31 15:34:07 -0400

    Fixed recommended category icon

--- wp-content/themes/modern-notion/partials/content-post-category-icon.php
+++ wp-content/themes/modern-notion/partials/content-post-category-icon.php
@@ -15,13 +15,16 @@ if($tax->taxonomy === 'category'){
 	$cat_slug = $tax->slug;
 }
 
-else if($tax->taxonomy === 'post_tag'){
+else{
 	$category = get_the_category($post_id)[0]; 
 	$cat_name = $category->cat_name; 
 	$cat_id = $category->term_id; 
 	$cat_url = get_category_link($cat_id);
 	$cat_slug = $category->slug; 
 }
+
+//var_dump($category); 
+
 $category_meta = get_option('category_meta');
 $href = '';
 $title_attr = '';
@@ -34,7 +37,7 @@ if($icon_element == 'a')  {
 	$title_attr = 'title="View all '.$cat_slug.' posts';
 }
 ?>
-<<?php echo $icon_element; ?> <?php echo $href; ?> class="valign halign post-icon category-icon <?php echo $cat_slug; ?>" style="background-color:<?php echo $category_meta[$cat_id]['color']; ?>;" <?php echo $title_attr; ?>>
+<<?php echo $icon_element; ?> <?php echo $href; ?> class="valign halign post-icon category-icon <?php echo $cat_slug;?>" style="background-color:<?php echo $category_meta[$cat_id]['color']; ?>;" <?php echo $title_attr; ?>>
 	<span>
 		<span class="icon-font"></span>
 		<span class="sr-only">
