9202f21f43b2fbe1cf90c6413763921930007de6
JP <jsniezek@crowdskout.com>
2014-08-04 09:53:27 -0400

    Added tinymce shortcode button

--- wp-content/themes/modern-notion/library/cite-photos.php
+++ wp-content/themes/modern-notion/library/cite-photos.php
@@ -51,4 +51,80 @@ function credit_photo($attrs, $content) {
 		return '<p class="cite-photo"><a href="'.$link.'" target="_blank">'.$author.'</a></p>';
 	}
 }
+
+new Shortcode_Tinymce();
+class Shortcode_Tinymce
+{
+    public function __construct()
+    {
+        add_action('admin_init', array($this, 'pu_shortcode_button'));
+        add_action('admin_footer', array($this, 'pu_get_shortcodes'));
+    }
+
+    /**
+     * Create a shortcode button for tinymce
+     *
+     * @return [type] [description]
+     */
+    public function pu_shortcode_button()
+    {
+        if( current_user_can('edit_posts') &&  current_user_can('edit_pages') )
+        {
+            add_filter( 'mce_external_plugins', array($this, 'pu_add_buttons' ));
+            add_filter( 'mce_buttons', array($this, 'pu_register_buttons' ));
+        }
+    }
+
+    /**
+     * Add new Javascript to the plugin scrippt array
+     *
+     * @param  Array $plugin_array - Array of scripts
+     *
+     * @return Array
+     */
+    public function pu_add_buttons( $plugin_array )
+    {
+        $plugin_array['pushortcodes'] = get_stylesheet_directory_uri() . '/library/js/shortcode-tinymce-button.js';
+
+        return $plugin_array;
+    }
+
+    /**
+     * Add new button to tinymce
+     *
+     * @param  Array $buttons - Array of buttons
+     *
+     * @return Array
+     */
+    public function pu_register_buttons( $buttons )
+    {
+        array_push( $buttons, 'separator', 'pushortcodes' );
+        return $buttons;
+    }
+
+    /**
+     * Add shortcode JS to the page
+     *
+     * @return HTML
+     */
+    public function pu_get_shortcodes()
+    {
+        //lobal $shortcode_tags;
+
+        echo '<script type="text/javascript">
+        var shortcodes_button = new Array();';
+
+        $count = 0;
+
+        echo "shortcodes_button[0] = 'credit'";
+
+        // foreach($shortcode_tags as $tag => $code)
+        // {
+        //     echo "shortcodes_button[{$count}] = '{$tag}';";
+        //     $count++;
+        // }
+
+        echo '</script>';
+    }
+}
 ?>
---
+++ wp-content/themes/modern-notion/library/js/shortcode-tinymce-button.js
@@ -0,0 +1,24 @@
+(function() {
+
+    tinymce.PluginManager.add('pushortcodes', function( editor )
+    {
+        var shortcodeValues = [];
+        jQuery.each(shortcodes_button, function(i)
+        {
+            shortcodeValues.push({text: shortcodes_button[i], value:i});
+        });
+
+        editor.addButton('pushortcodes', {
+            type: 'listbox',
+            text: 'Shortcodes',
+            onselect: function(e) {
+                var v = e.control._text;
+                if(v === 'credit') {
+                    var code = '[credit author="" link="" id=""]';
+                    tinyMCE.activeEditor.selection.setContent( code );
+                }
+            },
+            values: shortcodeValues
+        });
+    });
+})();
