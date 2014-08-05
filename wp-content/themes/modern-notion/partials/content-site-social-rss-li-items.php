f621765c0d6393c95fa0821adf4133237a587da0
JP <jsniezek@crowdskout.com>
2014-08-05 18:48:48 -0400

    Added social media fonts

--- wp-content/themes/modern-notion/partials/content-site-social-rss-li-items.php
+++ wp-content/themes/modern-notion/partials/content-site-social-rss-li-items.php
@@ -1,9 +1,21 @@
 <li>
-	<a href="https://twitter.com/modernnotion" target="_blank" class="twitter"></a>
+	<a target="_blank" href="https://twitter.com/modernnotion" class="icon-twitter-c"><span class="sr-only">Twitter</span></a>
 </li>
 <li>
-	<a href="https://www.facebook.com/modernnotion" target="_blank" class="facebook"></a>
+	<a target="_blank" href="https://facebook.com/modernnotion" class="icon-facebook-c"><span class="sr-only">Facebook</span></a>
+</li>
+<li>
+	<a target="_blank" href="http://instagram.com/modernnotion" class="icon-instagram-c"><span class="sr-only">Instagram</span></a>
+</li>
+<li>
+	<a target="_blank" href="http://www.pinterest.com/modernnotion/" class="icon-pinterest-c"><span class="sr-only">Pinterest</span></a>
+</li>
+<li>
+	<a target="_blank" href="https://vine.co/u/1085710024015339520" class="icon-vine-c"><span class="sr-only">Vine</span></a>
+</li>
+<li>
+	<a target="_blank" href="http://modernnotion.tumblr.com" class="icon-tumblr-c"><span class="sr-only">Vine</span></a>
 </li>						
 <li class="rss-feed-item">
-	<a href="<?php bloginfo('rss2_url'); ?>" title="RSS Feed" class="icon-feed"><span class="sr-only">RSS Feed</span></a>
+	<a target="_blank" href="<?php bloginfo('rss2_url'); ?>" title="RSS Feed" class="icon-feed"><span class="sr-only">RSS Feed</span></a>
 </li>
