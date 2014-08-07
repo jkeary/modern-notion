2ebe661b3fae4dafb17ff02011f0d7fd5deea46a
JP <jsniezek@crowdskout.com>
2014-08-07 08:47:23 -0400

    Put top articles into a table to align vertically

--- wp-content/themes/modern-notion/homepage.php
+++ wp-content/themes/modern-notion/homepage.php
@@ -37,17 +37,22 @@
 				<div id="main">
 					<div class="cf">
 						<div class="top headlining visible-lg">
+							<table>
 							<?php foreach($top as $post) : setup_postdata($post); $cat = get_the_category()[0]; ?>
+								<tr><td>
 								<article class="top-article">
 									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 									<p class="meta">
 										<a href="<?php echo get_category_link($cat->cat_ID);?>" style="color: <?php echo $category_meta[$cat->cat_ID]['color']?>">
 											<?php echo $cat->slug; ?>
-										</a> 
+										</a>
 										By <?php the_author_posts_link(); ?>
+									</p>
 									<div class="sep"></div>
 								</article>
-							<?php endforeach; ?>											
+								</td></tr>
+							<?php endforeach; ?>	
+							</table>										
 						</div>												
 						<div id="hero">
 							<?php foreach($hero as $post) : setup_postdata($post); $cat = get_the_category(); $cat = $cat[0]; ?>					
