753189c44b62ba8eba213092d9579996fbde06aa
JP <jsniezek@crowdskout.com>
2014-08-11 17:15:25 -0400

    Added dek and intro fields to single article page

--- wp-content/themes/modern-notion/partials/content-single-article.php
+++ wp-content/themes/modern-notion/partials/content-single-article.php
@@ -92,9 +92,14 @@
                 <div class="main">
                     <div class="content-wrapper">
                     <?php if ( has_post_format( 'aside' )): ?>
-                        <?php if(false): //if(get_field('dek')): ?>
+                        <?php if(get_field('dek')): ?>
                             <div class="prose dek category-prose <?php echo top_category_slug(); ?>-styled">
                                 <?php the_field('dek'); ?>
+                            </div>
+                        <?php endif; ?>                        
+                        <?php if(get_field('intro')): ?>
+                            <div class="prose dek category-prose <?php echo top_category_slug(); ?>-styled">
+                                <?php the_field('intro'); ?>
                             </div>
                         <?php endif; ?>                         
                         <?php $values = get_field('list_items'); ?>
