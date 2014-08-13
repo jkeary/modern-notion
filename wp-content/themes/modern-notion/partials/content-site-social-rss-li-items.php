124a5e575f6f08242a5335e62aee5d1ece14d2f0
JP <jsniezek@crowdskout.com>
2014-08-13 14:32:50 -0400

    Added icon fonts

--- wp-content/themes/modern-notion/partials/content-site-social-rss-li-items.php
+++ wp-content/themes/modern-notion/partials/content-site-social-rss-li-items.php
@@ -1,21 +1,21 @@
 <li>
-	<a target="_blank" href="https://twitter.com/modernnotion" class="icon-twitter-c"><span class="sr-only">Twitter</span></a>
+	<a target="_blank" href="https://twitter.com/modernnotion" class="icon-twitter2"><span class="sr-only">Twitter</span></a>
 </li>
 <li>
-	<a target="_blank" href="https://facebook.com/modernnotion" class="icon-facebook-c"><span class="sr-only">Facebook</span></a>
+	<a target="_blank" href="https://facebook.com/modernnotion" class="icon-facebook2"><span class="sr-only">Facebook</span></a>
 </li>
 <li class="instagram-item">
-	<a target="_blank" href="http://instagram.com/modernnotion" class="icon-instagram-c"><span class="sr-only">Instagram</span></a>
+	<a target="_blank" href="http://instagram.com/modernnotion" class="icon-instagram2"><span class="sr-only">Instagram</span></a>
 </li>
 <li class="pinterest-item">
-	<a target="_blank" href="http://www.pinterest.com/modernnotion/" class="icon-pinterest-c"><span class="sr-only">Pinterest</span></a>
+	<a target="_blank" href="http://www.pinterest.com/modernnotion/" class="icon-pinterest2"><span class="sr-only">Pinterest</span></a>
 </li>
 <li class="vine-item">
-	<a target="_blank" href="https://vine.co/u/1085710024015339520" class="icon-vine-c"><span class="sr-only">Vine</span></a>
+	<a target="_blank" href="https://vine.co/u/1085710024015339520" class="icon-vine2"><span class="sr-only">Vine</span></a>
 </li>
 <li class="tumblr-item">
-	<a target="_blank" href="http://modernnotion.tumblr.com" class="icon-tumblr-c"><span class="sr-only">Vine</span></a>
+	<a target="_blank" href="http://modernnotion.tumblr.com" class="icon-tumblr2"><span class="sr-only">Vine</span></a>
 </li>						
 <li class="rss-feed-item">
-	<a target="_blank" href="<?php bloginfo('rss2_url'); ?>" title="RSS Feed" class="icon-feed"><span class="sr-only">RSS Feed</span></a>
+	<a target="_blank" href="<?php bloginfo('rss2_url'); ?>" title="RSS Feed" class="icon-rss"><span class="sr-only">RSS Feed</span></a>
 </li>
